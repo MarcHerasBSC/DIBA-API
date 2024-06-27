15934e1cdaa0510bcc843e267542e438c019f144
MarcHerasBSC <marc.heras@bsc.es>
2024-06-27 15:01:40 +0200

    Update secret user and pwrd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { BooksService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,19 +7,25 @@ import { Book } from './book.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '84.88.187.146',
-      port: 5432,
-      username: 'david',
-      password: 'davidbsc',
-      database: 'DIBA-BSC',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: '84.88.187.146',
+        port: 5432,
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PWRD'),
+        database: 'DIBA-BSC',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService]
     }),
     TypeOrmModule.forFeature([Book])
   ],
   controllers: [AppController],
   providers: [BooksService],
 })
+
 export class AppModule {}
