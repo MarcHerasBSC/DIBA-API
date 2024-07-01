960a5533171dd45b03f3c71cea1c03dd3a27229e
MarcHeras <marc.heras@bsc.es>
2024-07-01 11:34:38 +0200

    Add app.controller.ts error handling

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, HttpException, HttpStatus } from '@nestjs/common';
 import { BooksService } from './app.service';
 
 @Controller('v1')
@@ -7,13 +7,24 @@ export class AppController {
 
   @Get('/books/:bookID')
   async getBookByID(@Param('bookID') bookID): Promise<string> {
-    const response = await this.appService.getRelatedBooks(bookID);
-    return response;
+    try {
+      const response = await this.appService.getRelatedBooks(bookID);
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Failed to get book recommendations by ID', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Get('/users/:userID')
   async getBookByUserID(@Param('userID') userID): Promise<string> {
-    const response = await this.appService.getRecommendedBooks(userID);
-    return response;
+    try {
+      const response = await this.appService.getRecommendedBooks(userID);
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Failed to get book recommendations by User ID', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
+
