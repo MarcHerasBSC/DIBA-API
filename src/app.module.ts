6e3e4ddf0df49985b18a0a3c5b7d0ecb1ee5ea9a
MarcHeras <marc.heras@bsc.es>
2024-07-01 11:22:35 +0200

    Add app.module.ts error handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,16 +10,23 @@ import { Book } from './book.entity';
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DATABASE_URL'),
-        port: configService.get('DATABASE_PORT'),
-        username: configService.get('DATABASE_USER'),
-        password: configService.get('DATABASE_PWRD'),
-        database: 'DIBA-BSC',
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
+      useFactory: async (configService: ConfigService) => {
+        try {
+          return {
+            type: 'postgres',
+            host: configService.get('DATABASE_URL'),
+            port: configService.get('DATABASE_PORT'),
+            username: configService.get('DATABASE_USER'),
+            password: configService.get('DATABASE_PWRD'),
+            database: 'DIBA-BSC',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+          };
+        } catch (error) {
+          console.error(error);
+          throw new Error('Failed to connect to the database');
+        }
+      },
       inject: [ConfigService]
     }),
     TypeOrmModule.forFeature([Book])
